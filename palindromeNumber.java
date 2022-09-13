19e9c350d4f2b922b40e15e0dea466febd70cf6e
davidukim <39535370+davidukim@users.noreply.github.com>
2022-09-12 23:42:06 -0400

    Delete palindromeNumber.java

--- palindromeNumber.java
+++
@@ -1,9 +0,0 @@
-public class palindromeNumber {
-    public boolean isPalindrome(int x) {
-        String s = Integer.toString(x);
-        for (int a = 0, b = s.length()-1; a < b; a++, b--){
-            if (s.charAt(a) != s.charAt(b)) return false;
-        }
-        return true;
-    }
-}
